50fcc86428999420e3169366bf643296215f1aab
JuanPLoaizaC <juanpablo9730@gmail.com>
2024-03-28 18:50:29 -0500

    Creating navbar for search with the characters

--- src/app/search/Search.tsx
+++ src/app/search/Search.tsx
@@ -1,7 +1,97 @@
-import React from 'react'
+import React from 'react';
 
 export const Search = () => {
+  const characters = [
+    {
+      name: 'Abadango Cluster Princess',
+      species: 'Alien',
+      status: 'Alive',
+      type: 'Princess',
+    },
+    {
+      name: 'Beth Smith',
+      species: 'Human',
+      status: 'Alive',
+      type: 'Character',
+    },
+    // Add more characters as needed
+  ];
+
+  const people = [
+    {
+      name: 'Leslie Alexander',
+      email: 'leslie.alexander@example.com',
+      role: 'Co-Founder / CEO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Michael Foster',
+      email: 'michael.foster@example.com',
+      role: 'Co-Founder / CTO',
+      imageUrl:
+        'https://images.unsplash.com/photo-1519244703995-f4e0f30006d5?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Dries Vincent',
+      email: 'dries.vincent@example.com',
+      role: 'Business Relations',
+      imageUrl:
+        'https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: null,
+    },
+    {
+      name: 'Lindsay Walton',
+      email: 'lindsay.walton@example.com',
+      role: 'Front-end Developer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Courtney Henry',
+      email: 'courtney.henry@example.com',
+      role: 'Designer',
+      imageUrl:
+        'https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: '3h ago',
+      lastSeenDateTime: '2023-01-23T13:23Z',
+    },
+    {
+      name: 'Tom Cook',
+      email: 'tom.cook@example.com',
+      role: 'Director of Product',
+      imageUrl:
+        'https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
+      lastSeen: null,
+    },
+  ]
+
   return (
-    <div>Search</div>
+    <>
+      <aside id="sidebar-multi-level-sidebar" className="fixed top-0 left-0 z-40 w-1/3 h-screen transition-transform -translate-x-full sm:translate-x-0" aria-label="Sidebar">
+        <div className="h-full px-3 py-4 overflow-y-auto bg-gray-50 dark:bg-gray-800">
+          <p className='mx-3 my-4 text-xl'>Rick and Morty list</p>
+          <ul role="list" className="divide-y divide-gray-100">
+            {people.map((person) => (
+              <li key={person.email} className="flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 dark:text-white dark:hover:bg-gray-700 ml-6 rounded-lg">
+                <div className="flex min-w-0 gap-x-4">
+                  <img className="h-10 w-10 flex-none rounded-full bg-gray-50 ml-5" src={person.imageUrl} alt="" />
+                  <div className="min-w-0 flex-auto">
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{person.name}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{person.email}</p>
+                  </div>
+                </div>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </aside>
+    </>
   )
 };
