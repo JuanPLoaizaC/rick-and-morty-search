8585cbe2e2681970316e4caf65d2d2f567d45c5b
JuanPLoaizaC <juanpablo9730@gmail.com>
2024-03-29 01:04:41 -0500

    Sorting by favorites and non-favorites

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -43,8 +43,40 @@ export const Search = () => {
               Z-A
             </button>
           </div>
+          <div className="flex ml-12">
+            <p className="text-sm leading-6 text-gray-600">STARRED CHARACTERS ({charactersList.filter(character => character.favourite).length})</p>
+          </div>
+          <ul role="list" className="divide-y divide-gray-100 mb-5">
+            {charactersList.filter(character => character.favourite).sort((a, b) => {
+              if (sortOrder === 'asc') {
+                return a.name.localeCompare(b.name);
+              } else if (sortOrder === 'desc') {
+                return b.name.localeCompare(a.name);
+              }
+            }).map((character) => (
+              <li key={character.id} className={`flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 ml-6 rounded-lg ${charactersSelected?.id === character.id && 'bg-purple-100'}`}>
+                <Link to={`/character/${character.id}`} onClick={() => setCharactersSelected(character)} className="flex items-center gap-x-4">
+                  <img className="h-10 w-10 rounded-full bg-gray-50 ml-5" src={character.image} alt="" />
+                  <div>
+                    <p className="text-sm font-semibold leading-6 text-gray-900">{character.name}</p>
+                    <p className="mt-1 truncate text-xs leading-5 text-gray-500">{character.species}</p>
+                  </div>
+                </Link>
+                <div className="flex items-center">
+                  <button className={`flex items-center justify-center rounded-full p-2 text-gray-800 mr-2 ${charactersSelected?.id === character.id ? 'bg-white' : 'bg-transparent'}`} onClick={() => changeFavourite(character.id)} >
+                    <svg xmlns="http://www.w3.org/2000/svg" className={`h-6 w-6 ${character.favourite ? '' : 'text-gray-300 stroke-current'}`} viewBox="0 0 20 20">
+                      <path fill={`${character.favourite ? 'green' : 'white'}`} stroke={`${character.favourite ? 'none' : 'currentColor'}`} strokeWidth="2" d="M10 18l-1-1.08C4.54 13.25 2 11.15 2 8.5 2 6.42 3.42 5 5.5 5c1.54 0 3.04.99 4 2.36C10.46 5.99 11.96 5 13.5 5 15.58 5 17 6.42 17 8.5c0 2.65-2.54 4.75-7 8.42L10 18z" clipRule="evenodd" />
+                    </svg>
+                  </button>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div className="flex ml-12">
+            <p className="text-sm leading-6 text-gray-600">CHARACTERS ({charactersList.filter(character => !character.favourite).length})</p>
+          </div>
           <ul role="list" className="divide-y divide-gray-100">
-            {charactersList.sort((a, b) => {
+            {charactersList.filter(character => !character.favourite).sort((a, b) => {
               if (sortOrder === 'asc') {
                 return a.name.localeCompare(b.name);
               } else if (sortOrder === 'desc') {
