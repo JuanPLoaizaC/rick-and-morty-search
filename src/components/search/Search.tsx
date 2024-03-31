e22022dd425e0b3ff5da53bd26d8d434ba3886b2
JuanPLoaizaC <juanpablo9730@gmail.com>
2024-03-31 17:39:57 -0500

    Can clic in all the card to redirect

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -128,7 +128,7 @@ export const Search = () => {
       <aside
         className={`fixed inset-y-0 left-0 z-40 w-full h-full bg-gray-50 overflow-y-auto sm:w-3/3 sm:relative sm:translate-x-0 ${manageCharacters.selectedCharacterId ? 'hide-on-mobile' : ''}`}
         aria-label="Sidebar"
-        style={{  }}
+        style={{}}
       >
         <div className="px-3 py-4 sm:px-4 sm:py-6">
           <p className="mx-3 my-4 text-xl font-bold">Rick and Morty list</p>
@@ -306,79 +306,89 @@ export const Search = () => {
                     }
                   })
                   .map((character) => (
-                    <li
-                      key={character.id}
-                      className={`flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 ml-6 rounded-lg ${manageCharacters?.selectedCharacterId === character.id &&
-                        "bg-purple-100"
-                        }`}
+                    <Link
+                      to={`/character/${character.id}`}
+                      onClick={() => {
+                        setCharactersSelected(character);
+                        manageCharacters.setSelectedCharacterId(character.id);
+                      }}
+                      className="items-center gap-x-4"
                     >
-                      <Link
-                        to={`/character/${character.id}`}
-                        onClick={() => {
-                          setCharactersSelected(character);
-                          manageCharacters.setSelectedCharacterId(character.id);
-                        }}
-                        className="flex items-center gap-x-4"
+                      <li
+                        key={character.id}
+                        className={`flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 ml-6 rounded-lg ${manageCharacters?.selectedCharacterId === character.id &&
+                          "bg-purple-100"
+                          }`}
                       >
-                        <img
-                          className="h-10 w-10 rounded-full bg-gray-50 ml-5"
-                          src={character.image}
-                          alt=""
-                        />
-                        <div>
-                          <p className="text-sm font-semibold leading-6 text-gray-900">
-                            {character.name}
-                          </p>
-                          <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                            {character.species}
-                          </p>
-                        </div>
-                      </Link>
-                      <div className="flex items-center">
                         <div className="flex items-center">
-                          <button
-                            className='flex items-center justify-center rounded-full p-2 text-gray-800' onClick={() => deleteCharacter(character.id)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              className="w-6 h-6"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                              />
-                            </svg>
-                          </button>
-                          <button
-                            className={`flex items-center justify-center rounded-full p-2 text-gray-800 mr-2 ${manageCharacters?.selectedCharacterId === character.id
-                              ? "bg-white"
-                              : "bg-transparent"
-                              }`}
-                            onClick={() => changeFavorite(character.id)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className={`h-6 w-6 ${character.favorite ? "" : "text-gray-300 stroke-current"
-                                }`}
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fill={`${character.favorite ? "green" : "white"}`}
-                                stroke={`${character.favorite ? "none" : "currentColor"}`}
-                                strokeWidth="2"
-                                d="M10 18l-1-1.08C4.54 13.25 2 11.15 2 8.5 2 6.42 3.42 5 5.5 5c1.54 0 3.04.99 4 2.36C10.46 5.99 11.96 5 13.5 5 15.58 5 17 6.42 17 8.5c0 2.65-2.54 4.75-7 8.42L10 18z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
+                          <img
+                            className="h-10 w-10 rounded-full bg-gray-50 ml-5"
+                            src={character.image}
+                            alt=""
+                          />
+                          <div>
+                            <p className="text-sm font-semibold leading-6 ml-3 text-gray-900">
+                              {character.name}
+                            </p>
+                            <p className="mt-1 truncate text-xs leading-5 ml-3 text-gray-500">
+                              {character.species}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </li>
+                        <div className="flex items-center">
+                          <div className="flex items-center">
+                            <button
+                              className='flex items-center justify-center rounded-full p-2 text-gray-800' onClick={(e) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                                deleteCharacter(character.id);
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                />
+                              </svg>
+                            </button>
+                            <button
+                              className={`flex items-center justify-center rounded-full p-2 text-gray-800 mr-2 ${manageCharacters?.selectedCharacterId === character.id
+                                ? "bg-white"
+                                : "bg-transparent"
+                                }`}
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                                changeFavorite(character.id);
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={`h-6 w-6 ${character.favorite ? "" : "text-gray-300 stroke-current"
+                                  }`}
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fill={`${character.favorite ? "green" : "white"}`}
+                                  stroke={`${character.favorite ? "none" : "currentColor"}`}
+                                  strokeWidth="2"
+                                  d="M10 18l-1-1.08C4.54 13.25 2 11.15 2 8.5 2 6.42 3.42 5 5.5 5c1.54 0 3.04.99 4 2.36C10.46 5.99 11.96 5 13.5 5 15.58 5 17 6.42 17 8.5c0 2.65-2.54 4.75-7 8.42L10 18z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                      </li>
+                    </Link>
                   ))}
               </ul>
             </>
@@ -418,7 +428,7 @@ export const Search = () => {
                       !character.deleted
                     );
                   })
-                  .sort((a, b) => {
+                  .sort((a: CharacterInterface, b: CharacterInterface) => {
                     if (sortOrder === "asc") {
                       return a.name.localeCompare(b.name);
                     } else if (sortOrder === "desc") {
@@ -426,83 +436,93 @@ export const Search = () => {
                     }
                   })
                   .map((character) => (
-                    <li
-                      key={character.id}
-                      className={`flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 ml-6 rounded-lg ${manageCharacters?.selectedCharacterId === character.id &&
-                        "bg-purple-100"
-                        }`}
+                    <Link
+                      to={`/character/${character.id}`}
+                      onClick={() => {
+                        setCharactersSelected(character);
+                        manageCharacters.setSelectedCharacterId(character.id);
+                      }}
+                      className="items-center gap-x-4"
                     >
-                      <Link
-                        to={`/character/${character.id}`}
-                        onClick={() => {
-                          setCharactersSelected(character);
-                          manageCharacters.setSelectedCharacterId(character.id);
-                        }}
-                        className="flex items-center gap-x-4"
+                      <li
+                        key={character.id}
+                        className={`flex justify-between gap-x-6 py-3.5 hover:bg-purple-100 ml-6 rounded-lg ${manageCharacters?.selectedCharacterId === character.id &&
+                          "bg-purple-100"
+                          }`}
                       >
-                        <img
-                          className="h-10 w-10 rounded-full bg-gray-50 ml-5"
-                          src={character.image}
-                          alt=""
-                        />
-                        <div>
-                          <p className="text-sm font-semibold leading-6 text-gray-900">
-                            {character.name}
-                          </p>
-                          <p className="mt-1 truncate text-xs leading-5 text-gray-500">
-                            {character.species}
-                          </p>
-                        </div>
-                      </Link>
-                      <div className="flex items-center">
                         <div className="flex items-center">
-                          <button
-                            className='flex items-center justify-center rounded-full p-2 text-gray-800' onClick={() => deleteCharacter(character.id)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              fill="none"
-                              viewBox="0 0 24 24"
-                              strokeWidth="1.5"
-                              stroke="currentColor"
-                              className="w-6 h-6"
-                            >
-                              <path
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
-                              />
-                            </svg>
-                          </button>
-                          <button
-                            className={`flex items-center justify-center rounded-full p-2 text-gray-800 mr-2 ${manageCharacters?.selectedCharacterId === character.id
-                              ? "bg-white"
-                              : "bg-transparent"
-                              }`}
-                            onClick={() => changeFavorite(character.id)}
-                          >
-                            <svg
-                              xmlns="http://www.w3.org/2000/svg"
-                              className={`h-6 w-6 ${character.favorite
-                                ? ""
-                                : "text-gray-300 stroke-current"
-                                }`}
-                              viewBox="0 0 20 20"
-                            >
-                              <path
-                                fill={`${character.favorite ? "green" : "white"
-                                  }`}
-                                stroke={`${character.favorite ? "none" : "currentColor"
-                                  }`}
-                                strokeWidth="2"
-                                d="M10 18l-1-1.08C4.54 13.25 2 11.15 2 8.5 2 6.42 3.42 5 5.5 5c1.54 0 3.04.99 4 2.36C10.46 5.99 11.96 5 13.5 5 15.58 5 17 6.42 17 8.5c0 2.65-2.54 4.75-7 8.42L10 18z"
-                                clipRule="evenodd"
-                              />
-                            </svg>
-                          </button>
+                          <img
+                            className="h-10 w-10 rounded-full bg-gray-50 ml-5"
+                            src={character.image}
+                            alt=""
+                          />
+                          <div>
+                            <p className="text-sm font-semibold leading-6 ml-3 text-gray-900">
+                              {character.name}
+                            </p>
+                            <p className="mt-1 truncate text-xs leading-5 ml-3 text-gray-500">
+                              {character.species}
+                            </p>
+                          </div>
                         </div>
-                      </div>
-                    </li>
+                        <div className="flex items-center">
+                          <div className="flex items-center">
+                            <button
+                              className='flex items-center justify-center rounded-full p-2 text-gray-800' onClick={(e) => {
+                                e.stopPropagation();
+                                e.preventDefault();
+                                deleteCharacter(character.id);
+                              }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                strokeWidth="1.5"
+                                stroke="currentColor"
+                                className="w-6 h-6"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  d="m14.74 9-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 0 1-2.244 2.077H8.084a2.25 2.25 0 0 1-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 0 0-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 0 1 3.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 0 0-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 0 0-7.5 0"
+                                />
+                              </svg>
+                            </button>
+                            <button
+                              className={`flex items-center justify-center rounded-full p-2 text-gray-800 mr-2 ${manageCharacters?.selectedCharacterId === character.id
+                                ? "bg-white"
+                                : "bg-transparent"
+                                }`}
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  e.preventDefault();
+                                  changeFavorite(character.id);
+                                }}
+                            >
+                              <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className={`h-6 w-6 ${character.favorite
+                                  ? ""
+                                  : "text-gray-300 stroke-current"
+                                  }`}
+                                viewBox="0 0 20 20"
+                              >
+                                <path
+                                  fill={`${character.favorite ? "green" : "white"
+                                    }`}
+                                  stroke={`${character.favorite ? "none" : "currentColor"
+                                    }`}
+                                  strokeWidth="2"
+                                  d="M10 18l-1-1.08C4.54 13.25 2 11.15 2 8.5 2 6.42 3.42 5 5.5 5c1.54 0 3.04.99 4 2.36C10.46 5.99 11.96 5 13.5 5 15.58 5 17 6.42 17 8.5c0 2.65-2.54 4.75-7 8.42L10 18z"
+                                  clipRule="evenodd"
+                                />
+                              </svg>
+                            </button>
+                          </div>
+                        </div>
+                      </li>
+                    </Link>
                   ))}
               </ul>
             </>
